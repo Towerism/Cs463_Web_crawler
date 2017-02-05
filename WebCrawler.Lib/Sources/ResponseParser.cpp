db865dfa4371e28b258533809d0426fab134ef4a
Martin Fracker <martin.frackerjr@gmail.com>
2017-02-05 15:19:26 -0600

    Optimize response parsing for arbitrary length content

--- WebCrawler.Lib/Sources/ResponseParser.cpp
+++ WebCrawler.Lib/Sources/ResponseParser.cpp
@@ -5,7 +5,7 @@
 #include "Networking.h"
 
 // Used regex101.com to build this regex and visualize the match.
-std::regex ResponseParser::response_regex = std::regex(R"((HTTP\/1.(0|1)\s*(\d{3})[\s\S]*?\r\n\s*\r\n)([\s\S]*)?)");
+std::regex ResponseParser::response_regex = std::regex(R"((HTTP\/1.(0|1)\s*(\d{3})[\s\S]*?\r\n\s*?\r\n))");
 
 ResponseParseResult* ResponseParser::Parse(std::string response)
 {
@@ -15,11 +15,19 @@ ResponseParseResult* ResponseParser::Parse(std::string response)
     return result;
   }
   std::smatch match_results;
-  result->Success = std::regex_match(response, match_results, response_regex);
+  result->Success = std::regex_search(response, match_results, response_regex);
   if (!result->Success)
     return result;
   result->Header = match_results[1];
   result->StatusCode = std::stoi(match_results[3]);
-  result->Content = match_results[4];
+
+  // parsing for content is a bit weird
+  // content can be arbitrarily long, plus there is no need to feed it through the regex library
+  // in some edge cases the string is too complex for the library to handle and it throws a regex_error
+  auto length = match_results[0].length();
+  std::string content(response);
+  content.erase(0, length);
+  result->Content = content;
+
   return result;
 }
